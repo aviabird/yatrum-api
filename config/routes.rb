5ab83da0520263c83f1b9fcf641bc65e78de76e5
Chandra Shekhar <shekharindian099@gmail.com>
2016-12-12 18:08:12 +0530

    changed routes for user trips

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'users/create'
 
   resources :trips
-  get ':user_id/trips', to: 'trips#get_user_trips'
+  get 'users/:user_id/trips', to: 'trips#get_user_trips'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   post 'authenticate', to: 'authentication#authenticate'

2fcfef5cbb8915bc7be4596ec49cdadaae70a225
Chandra Shekhar <shekharindian099@gmail.com>
2016-12-23 17:02:21 +0530

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,153 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Trip.destroy_all
 User.destroy_all
-u = User.create(name: 'Jack', email: 'jack@test.com', password: '123456789', password_confirmation: '123456789')
-# trip = u.trips.new(
-#   cities_attributes: [{
-#     name: 'Pune',
-#     country: 'India',
-#     places_attributes: [{
-#       name: 'Agakhan palace',
-#       description: 'A very nice place',
-#       review: 'A good review',
-#       pictures_attributes: [{
-#         url: 'http://placehold.it/200*200',
-#         description: 'just a pic'url: 'http://placehold.it/200*200'url: 'http://placehold.it/200*200'
-#       }]
-#     }, {
-#       name: 'Mulshi dam',
-#       description: 'A very serene place',
-#       review: 'Dont go alone, not very safe',
-#       pictures_attributes: [{
-#         url: 'http://placehold.it/200*200',
-#         description: 'just a pic'
-#       }]
-#     }]
-#   }]
-# )
+Trip.destroy_all
+City.destroy_all
+Place.destroy_all
+Picture.destroy_all
+
+user = User.create!(name: 'Jack', email: 'jack@test.com', password: '12345', password_confirmation: '12345')
+
 (1..10).each do |count|
-  trip = u.trips.create!(name: "Pune trip #{count}", description: "Was mostly in summer #{count}")
+  trip = user.trips.create!(name: "Pune trip #{count}", description: "Was mostly in summer #{count}")
   city = trip.cities.create!(name: "Pune #{count}", country: 'India')
-  place1 = city.places.create!(name: "Agakhan Palace #{count}", description: "A very nice place #{count}", review: 'A good review')
-  place2 = city.places.create!(name: "Mulshi Dam #{count}", description: "A very nice place #{count}", review: 'A good review')
-  place1.pictures.create!(url: 'https://unsplash.it/300/300/?random', description: 'just a pic')
-  place2.pictures.create!(url: 'https://unsplash.it/300/300/?random', description: 'just a pic')
+  place = city.places.create!(name: "Agakhan Palace #{count}", description: "A very nice place #{count}", review: 'A good review')
+  place.pictures.create!(url: 'https://unsplash.it/300/300/?random', description: 'just a pic')
+  place = city.places.create!(name: "Mulshi Dam #{count}", description: "A very nice place #{count}", review: 'A good review')
+  place.pictures.create!(url: 'https://unsplash.it/300/300/?random', description: 'just a pic')
 end
 
+user = User.create!(name: 'Chandra Shekhar', email: 'shekharait254@gmail.com', password: '12345', password_confirmation: '12345')
+trip = user.trips.create!(name: 'My first to India', description: 'Company Trip', status: 'completed')
+city = trip.cities.create!(name: 'Pune', country: 'India')
+place = city.places.create!(name: 'Aga khan Palace', description: 'old fort', review: 'very nice place')
+place.pictures.create!(url: "http://lorempixel.com/400/200", description: "Had so much fun here")
 
+trip = user.trips.create!(name: 'My first to Dubai', description: 'Dubai Trip', status: 'completed')
+city = trip.cities.create!(name: 'Pune', country: 'India')
+place = city.places.create!(
+  name: "Aga Khan Palace",
+  description: "The Aga Khan Palace was built by Sultan Muhammed Shah Aga Khan III in Pune, India. Built in 1892, 
+                it is one of the biggest landmarks in Indian history. The palace was an act of charity by the Sultan who wanted
+                to help the poor in the neighbouring areas of Pune, who were drastically hit by famine",
+  review: "Very nice palace"
+)
+place.pictures.create!(
+  url: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Pune_Palace.jpg/1024px-Pune_Palace.jpg",
+  description: "Outside Aga khan palace"
+)
+
+place.pictures.create!(
+  url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Gandhis_ashes.jpg/800px-Gandhis_ashes.jpg",
+  description: "Gandhi's ashes at the palace"
+)
+
+place = city.places.create!(
+  name: "Shaniwar Wada",
+  description: "Shaniwarwada (Śanivāravāḍā) is an 18th-century fortification in the city of Pune in Maharashtra, India. 
+                Built in 1732,[1] it was the seat of the Peshwa rulers of the Maratha Empire until 1818, when the Peshwas 
+                lost control to the East India Company after the Third Anglo-Maratha War. Following the rise of the Maratha Empire,
+                the palace became the center of Indian politics in the 18th century",
+  review: "Good place"
+)
+place.pictures.create!(
+  url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Shaniwarwada_gate.JPG/280px-Shaniwarwada_gate.JPG",
+  description: "Gate of shaniwar wada"
+)
+
+place.pictures.create!(
+  url: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Shaniwar_Wada_palace_fountain.JPG/220px-Shaniwar_Wada_palace_fountain.JPG",
+  description: "Shaniwar wada palace fountain"
+)
+
+place = city.places.create!(
+  name: "Hyatt Regency",
+  description: "This upscale hotel is a 4-minute walk from the nearest bus stop, 1.5 km from Aga Khan Palace and 8 km from the fortress of Shaniwar Wada.",
+  review: "Nice rooms with nice food"
+)
+place.pictures.create!(
+  url: "https://dubai.regency.hyatt.com/content/dam/PropertyWebsites/regency/dxbrd/Media/All/Hyatt-Regency-Dubai-P243-King-Room-with-City-and-Ocean-View.masthead-feature-panel-medium.jpg",
+  description: "My room interior"
+)
+place.pictures.create!(
+  url: "https://media-cdn.tripadvisor.com/media/photo-s/06/09/af/79/muslim-family-restaurant.jpg",
+  description: "Having lunch with friends"
+)
+
+place = city.places.create!(
+  name: "Lohagad fort",
+  description: "Lohagad (Marathi: लोहगड, iron fort) is one of the many hill forts of Maharashtra state in India. Situated close to the hill station Lonavala
+                and 52 km (32 mi) northwest of Pune, Lohagad rises to an elevation of 1,033 m (3,389 ft) above sea level. The fort is connected
+                to the neighboring Visapur fort by a small range. The fort was under the Maratha empire for the majority of time, with a short period of 5 years under the Mughal empire.",
+  review: "nice fort"
+)
+place.pictures.create!(
+  url: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Ganesh_Darwaja_Lohagad.jpg/800px-Ganesh_Darwaja_Lohagad.jpg",
+  description: "At Lohgad fort main entrance"
+)
+place.pictures.create!(
+  url: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Lohagad_wall.jpg/220px-Lohagad_wall.jpg",
+  description: "Lohgad fort walls"
+)
+
+city = trip.cities.create!(name: "Dubai", country: "UAE")
+place = city.places.create!(
+  name: "Burj Khalifa",
+  description: "The Burj Khalifa (Arabic: برج خليفة‎‎, Arabic for 'Khalifa Tower'; pronounced English /ˈbɜːrdʒ kəˈliːfə/),
+                known as the Burj Dubai before its inauguration, is a megatall skyscraper in Dubai, United Arab Emirates.
+                It is the tallest structure in the world, standing at 829.8 m ",
+  review: "Nice view from top of the building", 
+)
+place.pictures.create!(
+  url: "http://4.bp.blogspot.com/-9E_KTNVHeWU/VNGeKvoBZMI/AAAAAAAAMSc/6vjnkEaqxQw/s1600/Burj%2BKhalifa%2BPhoto%2Bat%2BNight%2B%2B03.jpg",
+  description: "Burj Khalifa at night"
+)
+place.pictures.create!(
+  url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Burj_Khalifa_001.jpg/1024px-Burj_Khalifa_001.jpg",
+  description: "Burj Khalifa NamePlate"
+)
+
+place = city.places.create!(
+  name: "Palm Islands", 
+  description: "Palm Islands are three artificial islands, Palm Jumeirah, Deira Island and Palm Jebel Ali, on the coast of Dubai,
+                United Arab Emirates. As of November 2014, only Palm Jumeirah has been completed. This island takes the form of a palm tree, 
+                topped by a crescent. After completion, Palm Jebel Ali will take a similar shape; each island will be host to a large number of 
+                residential, leisure and entertainment centers and will add a total of 520 kilometers of non-public beaches to the city of Dubai.",
+  review: "Very nice place to visit"
+)
+place.pictures.create!(
+  url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Palm_Jumeirah_early_evening_March_2015.jpg/1024px-Palm_Jumeirah_early_evening_March_2015.jpg",
+  description: "Early evening view of palm islands"
+)
+
+place = city.places.create!(
+  name: "The Dubai Mall",
+  description: "The Dubai Mall (Arabic: دبي مول‎‎) is a shopping mall in Dubai and the largest mall in the world by total area.
+                Along with West Edmonton Mall in Canada, it is the nineteenth largest shopping mall in the world by gross leasable area.
+                Located in Dubai, United Arab Emirates, it is part of the 20-billion-dollar Downtown complex, and includes 1,200 shops.
+                In 2011 it was the most visited building on the planet, attracting over 54 million visitors. Access to the mall is provided 
+                via Doha Street, rebuilt as a double-decker road in April 2009",
+  review: "Expensive shopping and leisue complex"
+)
+place.pictures.create!(
+  url: "https://i.ytimg.com/vi/1XnYjReHyT0/hqdefault.jpg",
+  description: "Inside view of Dubai Mall"
+)
+place.pictures.create!(
+  url: "http://c8.alamy.com/comp/D178H3/united-arab-emirates-uae-uae-middle-east-dubai-downtown-dubai-dubai-D178H3.jpg",
+  description: "too much rush outside KFC"
+)
+
+place = city.places.create!(
+  name: "Ski Dubai",
+  description: "Ski Dubai is an indoor ski resort with 22,500 square meters of indoor ski area.
+                It is a part of the Mall of the Emirates, one of the largest shopping malls in the world, located in Dubai,
+                United Arab Emirates. It was developed by Majid Al Futtaim Group, which also operates the Mall of the Emirates.",
+  review: "Good Place for Indoor Activites at Dubai"
+)
+place.pictures.create!(
+  url: "http://www.malloftheemirates.com/-/media/malloftheemirates/entertainment/skidubaii.jpg",
+  description: "Skiing with the family and friends"
+)
+place.pictures.create!(
+  url: "https://booking.skidxb.com/sites/default/files/styles/list-style/public/package_image/ski-school_0.jpg?itok=ZfZL10J5",
+  description: "Having fun with friends"
+)

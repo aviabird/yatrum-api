ff137eab5a6d63800a515fd7870b91718e6a7d27
Ashish Singh <aitashish173@gmail.com>
2016-12-27 17:22:05 +0530

    added required false

--- app/models/city.rb
+++ app/models/city.rb
@@ -12,8 +12,8 @@
 
 class City < ApplicationRecord
   has_many :places
-  belongs_to :trip
+  belongs_to :trip, required: false # http://stackoverflow.com/a/39584972/1930053
 
-  # accepts_nested_attributes_for :places
+  accepts_nested_attributes_for :places
 
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -13,8 +13,8 @@
 
 class Place < ApplicationRecord
   has_many :pictures
-  belongs_to :city
+  belongs_to :city, required: false # http://stackoverflow.com/a/39584972/1930053
 
-  # accepts_nested_attributes_for :pictures
+  accepts_nested_attributes_for :pictures
 
 end

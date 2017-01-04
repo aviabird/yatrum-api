55050d6ed6a1002f418dacc462543219fe6856a5
Ashish Singh <aitashish173@gmail.com>
2017-01-04 22:46:57 +0530

    updated create method

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,5 @@
 class TripsController < ApplicationController
-  # before_action :authenticate_request, only: [:create, :update, :destroy]
+  before_action :authenticate_request, only: [:create, :update, :destroy]
   before_action :set_trip, only: [:show, :update, :destroy]
 
   # GET /trips
@@ -15,7 +15,7 @@ class TripsController < ApplicationController
 
   # POST /trips
   def create
-    @trip = Trip.new(trip_params)
+    @trip = current_user.trips.new(trip_params)
     if @trip.save
       render json: @trip, status: :created, location: @trip
     else

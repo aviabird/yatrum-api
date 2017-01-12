38eeb7c79046b0d40fb8610842deb60504d30393
rawat <rawat@rawats-iMac.local>
2017-01-12 21:50:38 +0530

    route update

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # get 'users/create'
 
-  get 'trips/search', to: 'trips#search'
   resources :trips
   get 'users/:user_id/trips', to: 'trips#get_user_trips'
+  post 'trips/search', to: 'trips#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   post 'authenticate', to: 'authentication#authenticate'

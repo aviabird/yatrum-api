2a3dc58fd120590e663c13de9b078b4db8f805de
Pankaj Rawat <rawat@Rawats-MacBook-Air.local>
2017-01-13 12:18:40 +0530

    added voting gem

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -10,7 +10,20 @@ class TripsController < ApplicationController
       .limit(10)
       .offset(params[:page] || 0)
 
-    render json: @trips
+    render json: Oj.dump(
+      @trips.as_json(
+        include: [
+          :user,
+          cities: {
+            include: [
+              places: {
+                include: :pictures
+              }
+            ]
+          }
+        ]
+      )
+    )
   end
 
   # GET /trips/1
@@ -45,8 +58,28 @@ class TripsController < ApplicationController
   # GET /users/:user_id/trips
   def get_user_trips
     user = User.find(params[:user_id])
-    trips = user.trips.includes(cities: [places: :pictures])
-    render json: trips
+    trips =
+      user
+      .trips
+      .includes(cities: [places: :pictures])
+      .order(created_at: :desc)
+      .offset(params[:page])
+      .limit(10)
+
+    render json: Oj.dump(
+      trips.as_json(
+        include: [
+          :user,
+          cities: {
+            include: [
+              places: {
+                include: :pictures
+              }
+            ]
+          }
+        ]
+      )
+    )
   end
 
   # GET /trips/search
@@ -57,9 +90,22 @@ class TripsController < ApplicationController
       .tagged_with(params[:keywords].try(:split), any: true)
       .order(created_at: :desc)
       .offset(params[:page])
-      .limit(20)
+      .limit(10)
       
-    render json: @trips
+    render json: Oj.dump(
+      @trips.as_json(
+        include: [
+          :user,
+          cities: {
+            include: [
+              places: {
+                include: :pictures
+              }
+            ]
+          }
+        ]
+      )
+    )
   end  
   
   private

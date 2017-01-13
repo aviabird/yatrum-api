f3678376a213e6976aa0a53c7ddc65d33e9ab0f6
Chandra Shekhar <shekharindian099@gmail.com>
2017-01-13 10:43:21 +0530

    gemfile update

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(version: 20170110144353) do
     t.integer  "city_id"
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
+    t.index ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
+  end
+
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
     t.string   "taggable_type"

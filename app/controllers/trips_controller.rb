c7ff12fa2e44af49f958199097ee6d0f89bb0c6d
Ashish Singh <aitashish173@gmail.com>
2017-01-16 14:08:25 +0530

    sanitised params for trip create/update

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,7 @@
 class TripsController < ApplicationController
   before_action :authenticate_request, only: [:create, :update, :destroy]
   before_action :set_trip, only: [:show, :update, :destroy]
+  before_action :sanitise_params, only: [:create, :update]
 
   # GET /trips
   def index
@@ -31,6 +32,7 @@ class TripsController < ApplicationController
   # PATCH/PUT /trips/1
   def update
     if @trip.update(trip_params)
+      # binding.pry
       render json: @trip
     else
       render json: @trip.errors, status: :unprocessable_entity
@@ -71,10 +73,23 @@ class TripsController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def trip_params
+    
     params.require(:trip).permit(:id, :name, :description, :status, :start_date, :end_date,
       cities_attributes: [:id, :name, :country, 
         places_attributes: [:id, :name, :description, :review]
       ]
     )
+  end
+
+  def sanitise_params
+    params['trip']['cities_attributes'] = params['trip']['cities']
+    # params['trip'].delete('cities')
+    params['trip']["cities_attributes"].each do |city|
+      city['places_attributes'] = city['places']
+      # city.delete('places')
+      city.values_at('places_attributes').each do |place|
+        # Change for picture here
+      end
+    end
   end
 end

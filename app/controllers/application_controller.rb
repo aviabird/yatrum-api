9f6ccf163c9b894f4854395b196d443805cf2de9
rawat <rawat@rawats-iMac.local>
2017-01-30 23:12:15 +0530

    current user fixes for ios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,19 @@
 class ApplicationController < ActionController::API
   include Authenticable
   include Common  
-  # before_action :authenticate_request 
+  
+  before_action :authenticate_api
   # This authentication should be at the controller level
 
-  # attr_reader :current_user 
-  # private
+  attr_reader :current_user 
+  private
 
-  # def authenticate_request 
-  #   @current_user = AuthorizeApiRequest.call(request.headers).result 
-  #   render json: { error: 'Not Authorized' }, status: 401 unless @current_user 
-  # end
+  def authenticate_api
+    @current_user = AuthorizeApiRequest.call(request.headers).result
+    Thread.current[:current_user] = @current_user if @current_user
+  end
 
-  # def user_signed_in?
-  #   current_user.present?
-  # end
+  def user_signed_in?
+    current_user.present?
+  end
 end

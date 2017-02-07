0217f9d3fe655ed80fe891f82407f138aeb5db32
Chandra Shekhar <shekharindian099@gmail.com>
2017-02-07 12:11:24 +0530

    trip serializer bug fix

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -35,7 +35,7 @@ class TripSerializer < ActiveModel::Serializer
   end
 
   def thumbnail_image_url
-    object.cities.first.places.first.pictures.first.url
+    object.places.first.pictures.first.url
   rescue
     "http://res.cloudinary.com/zeus999/image/upload/h_300/v1483437708/sea-sky-beach-holiday-11_nnbuey.jpg" 
   end

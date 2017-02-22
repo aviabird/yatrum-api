7d0bd091a143c2af6abb4a051b926f561d45667a
Chandra Shekhar <shekharindian099@gmail.com>
2017-02-22 19:15:21 +0530

    pull changes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,37 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                        :integer          not null, primary key
+#  name                      :string
+#  email                     :string           default(""), not null
+#  created_at                :datetime         not null
+#  updated_at                :datetime         not null
+#  instagram_access_token    :string
+#  instagram_user_name       :string
+#  instagram_profile_picture :string
+#  profile_pic               :text
+#  cover_photo               :text
+#  google                    :string
+#  facebook                  :string
+#  display_name              :string
+#  role_id                   :integer
+#  encrypted_password        :string           default(""), not null
+#  reset_password_token      :string
+#  reset_password_sent_at    :datetime
+#  remember_created_at       :datetime
+#  sign_in_count             :integer          default(0), not null
+#  current_sign_in_at        :datetime
+#  last_sign_in_at           :datetime
+#  current_sign_in_ip        :inet
+#  last_sign_in_ip           :inet
+#  facebook_url              :string
+#  twitter_url               :string
+#  instagram_url             :string
+#  website_url               :string
+#  blog_url                  :string
+#
+
 
 require 'test_helper'
 

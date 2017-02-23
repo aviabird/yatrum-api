704e17cfbe3b953773bd09e8dc7a703e161daee7
Chandra Shekhar <shekharindian099@gmail.com>
2017-02-24 01:49:38 +0530

    add visited_date method to trip_serializer

--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -16,4 +16,13 @@ class PlaceSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :review, :visited_date, :created_at, :updated_at
 
   has_many :pictures
+
+  def visited_date
+    if(object.visited_date)
+      object.visited_date
+    else
+      object.created_at
+    end
+  end
+
 end
